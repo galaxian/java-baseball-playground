41a93218ec3b026e1c7730993da61b2edf662fe2
Lee <94841127+galaxian@users.noreply.github.com>
2022-07-23 15:19:28 +0900

    test : 테스트코드 리팩토링

--- src/test/java/baseball/ProcessBaseballTest.java
+++ src/test/java/baseball/ProcessBaseballTest.java
@@ -3,7 +3,7 @@ package baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,10 +19,7 @@ public class ProcessBaseballTest {
     @Test
     void countStrikeTest() {
         String input = "123";
-        List<Integer> ball = new ArrayList<>();
-        ball.add(1);
-        ball.add(3);
-        ball.add(9);
+        List<Integer> ball = Arrays.asList(1, 3, 9);
 
         assertThat(processBaseball.cntStrike(input.charAt(0), ball.get(0))).isEqualTo(1);
     }
@@ -30,10 +27,7 @@ public class ProcessBaseballTest {
     @Test
     void countBallTest(){
         String input = "123";
-        List<Integer> ball = new ArrayList<>();
-        ball.add(1);
-        ball.add(3);
-        ball.add(9);
+        List<Integer> ball = Arrays.asList(1, 3, 9);
 
         assertThat(processBaseball.cntBall(input.charAt(0), ball)).isEqualTo(1);
     }
@@ -41,10 +35,7 @@ public class ProcessBaseballTest {
     @Test
     void checkStrikeTest() {
         String input = "123";
-        List<Integer> ball = new ArrayList<>();
-        ball.add(1);
-        ball.add(3);
-        ball.add(9);
+        List<Integer> ball = Arrays.asList(1, 3, 9);
 
         assertThat(processBaseball.checkStrike(input, ball)).isEqualTo(1);
     }
@@ -52,10 +43,7 @@ public class ProcessBaseballTest {
     @Test
     void checkBallTest() {
         String input = "193";
-        List<Integer> ball = new ArrayList<>();
-        ball.add(1);
-        ball.add(3);
-        ball.add(9);
+        List<Integer> ball = Arrays.asList(1, 3, 9);
 
         assertThat(processBaseball.checkBall(input, ball)).isEqualTo(2);
     }
@@ -63,10 +51,7 @@ public class ProcessBaseballTest {
     @Test
     void resultTest() {
         String input = "193";
-        List<Integer> ball = new ArrayList<>();
-        ball.add(1);
-        ball.add(3);
-        ball.add(9);
+        List<Integer> ball = Arrays.asList(1, 3, 9);
 
         assertThat(processBaseball.result(input, ball)).isEqualTo("2볼 1스트라이크");
     }
@@ -74,10 +59,7 @@ public class ProcessBaseballTest {
     @Test
     void answerTest() {
         String input = "123";
-        List<Integer> ball = new ArrayList<>();
-        ball.add(1);
-        ball.add(2);
-        ball.add(3);
+        List<Integer> ball = Arrays.asList(1, 2, 3);
 
         assertThat(processBaseball.resultView(input, ball)).isFalse();
     }
