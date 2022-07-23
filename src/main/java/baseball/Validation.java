8fec418fb450980d19191a49d0ef2314350c8d4f
Lee <94841127+galaxian@users.noreply.github.com>
2022-07-23 23:56:59 +0900

    feat : 3자리 숫자 중 중복 유무를 판단하는 기능 구현

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Validation {
     public boolean validRange(int number) {
         return number < 10 && number > 0;
+    }
+
+    public boolean duplicate(List<Integer> asList) {
+        return new HashSet<>(asList).size() != 3;
     }
 }
