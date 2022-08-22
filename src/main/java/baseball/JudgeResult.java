676f363dfa19ca6848532896eb7d55f6115b3807
Lee <94841127+galaxian@users.noreply.github.com>
2022-08-22 19:23:53 +0900

    feat : 게임 결과를 리턴하는 기능 추가

--- src/main/java/baseball/JudgeResult.java
+++ src/main/java/baseball/JudgeResult.java
@@ -20,4 +20,21 @@ public class JudgeResult {
             ball++;
         }
     }
+
+    public String ballsReport() {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        if (strike == 0) {
+            return ball + " 볼";
+        }
+        if (ball == 0) {
+            return strike + " 스트라이크";
+        }
+        return ball + " 볼," + strike +" 스트라이크";
+    }
+
+    public boolean isGameEnd() {
+        return strike == 3;
+    }
 }
